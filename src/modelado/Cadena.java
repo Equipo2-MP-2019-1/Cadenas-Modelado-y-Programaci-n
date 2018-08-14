3a353ea557dfa0ea5890d1370ee50dc96d43f089
Cesar Farrera <cfarrera@nerds.ai>
2018-08-14 12:59:15 -0500

    [FEATURE] esqueleto de clases

---
+++ src/modelado/Cadena.java
@@ -0,0 +1,25 @@
+
+package modelado.cadenas;
+
+public class Cadena {
+
+    public String concatenacion(String s1, String s2){
+        return "";
+    }
+
+    public String reversa(String s){
+        return "";
+    }
+
+    public boolean palindromo(String s){
+        return true;
+    }
+
+    public String repetidos(String s, char c){
+        return "";
+    }
+
+    public String quitaEspacios(String s){
+        return "";
+    }
+}
---
+++ test/CadenaTest.java
@@ -0,0 +1,28 @@
+/*
+ * Código utilizado para el curso de Estructuras de Datos.
+ * Se permite consultarlo para fines didácticos en forma personal.
+ */
+
+package modelado.cadenas;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+import java.util.Random;
+
+/**
+ * Batería de pruebas unitarias para la clase <code>Vector</code>.
+ * @author veronica
+ */
+public class CadenaTest {
+
+    private Random intGenerator;
+    private static float mark;
+
+    public CadenaTest() {
+        intGenerator = new Random();
+    }
+
+}
